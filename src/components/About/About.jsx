eab0842fc6d359d191406313e2bf658bbdf56e40
Trinidad Padilla <tpadilla101@live.com>
2021-10-19 17:53:39 -0700

    finished the About.jsx component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './About.css';
 
+import Award from '../../assets/award_img.png';
+
 const About = () => {
     return (
         <div className="about">
@@ -18,7 +20,19 @@ const About = () => {
             </div>
 
             <div className="about-right">
-hey
+                <h1 className="about-title"> About Me</h1>
+                <p className="about-sub">It is a long established fact taht a reader will be distracted by the readable content.</p>
+                <p className="about-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+                <div className="about-award">
+                    <img className="about-award-img" src={Award} alt=""/>
+                    <div className="about-award-texts">
+                        <h4 className="about-award-title">International Design</h4>
+                        <p className="about-award-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
